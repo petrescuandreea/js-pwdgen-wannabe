b04d7a7f1f88d7e6f9f0350148e9881805288b5a
Andreea1696 <andreeapetrescu16@gmail.com>
2021-10-20 15:29:47 +0200

    pasword generator done

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// password generator 
+
+// chiediamo all'utente il suo nome 
+let firstName = prompt("Inserisci qui il tuo nome");
+// console.log("Il nome dell'utente è: " , (firstName));
+
+
+// chiediamo all'utente il suo cognome 
+let lastName = prompt("Inserisci qui il tuo cognome");
+// console.log("Il cognome dell'utente è: " , (lastName));
+
+
+// chiediamo all'utente il suo colore preferito
+let favColor = prompt("Inserisci qui il tuo colore preferito");
+// console.log("Il colore preferito dell'utente è: " , (favColor));
+
+
+// ricaviamo la password 
+let pasword = `${firstName}${lastName}${favColor}21`;
+// console.log ("la password generata è: " , (pasword));
+
+
+// facciamo vedere la password all'utente
+document.getElementById("mio-id").innerHTML = `La password assegnata è: ${pasword}`;
